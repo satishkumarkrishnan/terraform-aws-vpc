fff05eb73427ebbc464dd035d59119921a567343
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-09-22 16:42:58 +0530

    updated code

--- main.tf
+++ main.tf
@@ -12,11 +12,33 @@ terraform {
 provider "aws" {
   region = "ap-northeast-1"
 }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "terraform-sg-example-satish"
+  }
+}
 resource "aws_instance" "example" {
   ami           = "ami-0a2e10c1b874595a1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
-    Name = "terraform-example"
+    Name = "terraform-example-satish"
   }
 }
