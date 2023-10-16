3ab536f405afac4e074b7e6bb184fa6c35fda2e9
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-16 20:16:06 +0530

    changed the version ami id

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-073fb7c9a83a77d26"
+  ami           = "ami-0f2e255ec956ade7f"
   instance_type = "t2.micro"
 
   tags = {
