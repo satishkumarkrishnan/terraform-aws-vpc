d975f7e3cfe8bfcd5fcefb8571d9cd4c36934f41
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-16 21:14:25 +0530

    Removed main.tf and renamed vpc.tf to main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,136 @@
-terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
+#######################################################
+################### Start - AWS VPC ###################
+#######################################################
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+resource "aws_vpc" "mumbai-vpc" {
+  cidr_block           = var.vpc_CIDR
+  instance_tenancy     = var.instanceTenancy
+  enable_dns_support   = var.dnsSupport
+  enable_dns_hostnames = var.dnsHostNames
+  tags = {
+    Name = var.vpc
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "mumbai-igw" {
+  vpc_id = aws_vpc.mumbai-vpc.id
+  tags = {
+    Name = var.internet-gateway
+  }
+}
+
+# Create Route Tables
+resource "aws_route_table" "mumbai-public-route" {
+  vpc_id = aws_vpc.mumbai-vpc.id
+  tags = {
+    Name = var.route-table["public"]
+  }
+}
+
+# Create Internet route access
+resource "aws_route" "mumbai-internet-route" {
+  route_table_id         = aws_route_table.mumbai-public-route.id
+  destination_cidr_block = var.allIPsCIDRblock
+  gateway_id             = aws_internet_gateway.mumbai-igw.id
+}
+
+# Create Subnets
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet)
+  vpc_id                  = aws_vpc.mumbai-vpc.id
+  cidr_block              = var.private_subnet[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = var.mapPublicIP
+  tags = {
+    Name        = "mumbai-subnets-${count.index}"
+  }
+}
+
+# Create Network Access Control Lists
+resource "aws_network_acl" "mumbai-nacl" {
+  count  = length(var.private_subnet)
+  vpc_id = aws_vpc.mumbai-vpc.id
+  subnet_ids = [aws_subnet.private[count.index].id]
+    # allow ingress HTTP from port  80 all IPs
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = var.allIPsCIDRblock
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  # allow egress ephemeral ports to all IPs
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = var.allIPsCIDRblock
+    from_port  = 0
+    to_port    = 65535
+  }
+    tags = {
+      Name = "mumbai-sg-${count.index}"
     }
   }
-}
 
-provider "aws" {
-  region = "ap-south-1"
-}
+# Create Security Groups
+resource "aws_security_group" "mumbai-securitygroup" {
+  count  = 2
+  vpc_id = aws_vpc.mumbai-vpc.id
 
-resource "aws_instance" "example" {
-  ami           = "ami-0f2e255ec956ade7f"
-  instance_type = "t2.micro"
+  # allow ingress HTTP from port  80 all IPs
+  ingress {
+    cidr_blocks = [var.allIPsCIDRblock]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
+  # allow ingress HTTPS port 443 from all IPs
+  ingress {
+    cidr_blocks = [var.allIPsCIDRblock]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
+
+  # allow ingress HTTPS port 22 from all IPs
+  ingress {
+    cidr_blocks = [var.allIPsCIDRblock]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+  # allow egress ephemeral ports to all IPs
+  egress {
+    cidr_blocks = [var.allIPsCIDRblock]
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+  }
   tags = {
-    Name = "terraform-example"
+    Name = "mumbai-sg-${count.index}"
   }
 }
+
+# Associate Route Tables with Subnets
+resource "aws_route_table_association" "mumbai-rt-association" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.mumbai-public-route.id
+}
+
+#####################################################
+################### End - AWS VPC ###################
+#####################################################
