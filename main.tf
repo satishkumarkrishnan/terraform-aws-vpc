a3c67b8b9a113076c3c0fc4b65bda5f8ae9163b6
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-21 20:51:57 +0530

    adding windows AMI

--- main.tf
+++ main.tf
@@ -148,8 +148,8 @@ resource "aws_security_group" "tokyo-securitygroup" {
   # Allow ingress and egress for Data Lineage
   ingress {
   description = "Data Lineage Port"
-  from_port   = 5000
-  to_port     = 5000
+  from_port   = 8000
+  to_port     = 8000
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   }
